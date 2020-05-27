7bc9f1d64fba57f885d7c6a246e0b888b3585051
juliatileo <juliatileo09@gmail.com>
2020-05-27 18:55:29 -0300

    deploy

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express')
-
 const app = express()
+const cors = require('cors')
 
 const routes = require('./src/routes')
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 app.use(express.static('src/uploads'))
-app.use((request, response, next) => {
-    response.header('Acess-Controll-Allow-Origin', '*')
-    response.header('Acess-Controll-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-})
 app.use((error, request, response, next) => {
     response.status(error.status || 500)
     response.json({ error: error.message })
