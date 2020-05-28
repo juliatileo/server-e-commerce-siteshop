42bff43463b9b31ce632fecc49981821c0fea2aa
juliatileo <juliatileo09@gmail.com>
2020-05-28 13:00:58 -0300

    arrumando venda

--- src/controllers/VendasController.js
+++ src/controllers/VendasController.js
@@ -12,13 +12,14 @@ module.exports = {
     },
     async createVenda(req, res, next) {
         try {
-            const { comprador_id, produto_id, vendedor_id, preco } = req.body
+            const { comprador_id, produto_id, vendedor_id, carrinho_id, preco } = req.body
             const comprador_data = await knex('users').select('users.creditos', 'users.compras').where('id', comprador_id)
             const vendedor_data = await knex('users').select('users.creditos', 'users.vendas').where('id', vendedor_id)
             const perda_comprador = comprador_data[0].creditos - preco
             const lucro_vendedor = vendedor_data[0].creditos + preco
             const compras = comprador_data[0].compras + 1
             const vendas = vendedor_data[0].vendas + 1
+            const carrinho = await knex('carrinhos').del().where('id', carrinho_id)
             const venda = await knex('vendas').insert({ comprador_id, produto_id, vendedor_id, preco })
             const comprador = await knex('users').where('id', comprador_id).update({ creditos: perda_comprador, compras })
             const vendedor = await knex('users').where('id', vendedor_id).update({ creditos: lucro_vendedor, vendas })
